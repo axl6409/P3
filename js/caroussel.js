b798841abf0c8ae86314c01588ef1a7c799b19ed
axl6409 <alexandre.celier64@gmail.com>
2019-06-14 00:12:41 +0200

    Comments on carousel.js, some fix on index.html

--- js/caroussel.js
+++ js/caroussel.js
@@ -1,55 +1,65 @@
 // Caroussel JS
 
-// Objet Caroussel
+// Class Caroussel
 class Caroussel {
 
 	/**
-	 * @param (HTMLElement) element
-	 * @param (Object) options
-	 * @param (Object) options.slidesToScroll Nombres d'éléments à faire défiler
-	 * @param (Object) options.slidesVisible Nombres d'éléments visibles dans un slide
+	 * @param {HTMLElement} element
+	 * @param {Object} options
+	 * @param {Object} options.slidesToScroll Nombres d'éléments à faire défiler
+	 * @param {Object} options.slidesVisible Nombres d'éléments visibles dans un slide
 	*/
 
-	// Methode
+	// Methode avec deux paramètres ou objet vide
 	constructor (element, options = {}) {
-		this.element = element
-		this.options = Object.assign({}, { 
-			slidesToScroll: 1,
+
+		this.element = element // sauvegarde l'élément dans une variable "element"
+		this.options = Object.assign({}, { // Création de la propriété "options" assigné à l'objet et les valeurs par default
+			slidesToScroll: 1, // Propriétés par défault
 			slidesVisible: 1
 		}, options)
+
 		this.children = [].slice.call(element.children)
 		let ratio = this.children.lenght / this.options.slidesVisible
-		let root = this.createDivWithClass('carousel')
-		let container = this.createDivWithClass('carousel__container')
+		let root = this.createDivWithClass('carousel') // Création d'une div avec la class carousel
+		let container = this.createDivWithClass('carousel__container') // Création d'une div avec la class carousel__container
 		container.style.width = ( ratio * 100 ) + "%"
-		root.appendChild(container)
-		this.element.appendChild(root)
+		root.appendChild(container) // Insère la DIV carousel__container dans la DIV carousel
+		this.element.appendChild(root) // Crée une DIV avec l'élément "root" dans l'élément #blocCarousel
 		this.children.forEach((child) => {
 			let item = this.createDivWithClass('carousel__item')
 			item.style.width = ((100 / this.options.slidesVisible) / ratio ) + "%"
 			item.appendChild(child)
 			container.appendChild(item)
 		})
-		debugger
 	}
 
 
+
+
+	// Création de la methode pour créer mes div
+
 	/**
-	 * @param (string) className
-	 * @returns (HTMLElement)
+	 * @param {string} className
+	 * @returns {HTMLElement}
 	*/
 
+	// Parametre nom de CLASSs
 	createDivWithClass (className) {
-		let div = document.createElement('div')
-		div.setAttribute('class', className)
+
+		let div = document.createElement('div') // Ajoute un élément HTML de type DIV
+		div.setAttribute('class', className) // lui ajoute l'attribut CLASS et en parametre de le nom de la CLASS
 		return div
 	}
 
 }
 
+// Attend le chargement du DOM
 document.addEventListener('DOMContentLoaded', function () {
 
-	new Caroussel(document.querySelector('#carousel'), {
+	// Création de la class carousel
+	new Caroussel(document.querySelector('#blocCarousel'), {
+		// Indique le nombre d'élément à faire defiler
 		slidesToScroll: 3,
 		slidesVisible: 3
 	})
